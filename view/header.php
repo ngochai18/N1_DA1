89e606a094a6a084cb30a928cbe167fae1960332
maivandung08112000 <maivandung08112000@gmail.com>
2024-07-21 20:10:19 +0700

    header, footer, hamo trong view

---
+++ view/header.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+
+<head>
+	<!-- Meta Data -->
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Home Clean | Ruper</title>
+
+	<!-- Favicon -->
+	<link rel="shortcut icon" type="image/x-icon" href="media/favicon.png">
+
+	<!-- Dependency Styles -->
+	<link rel="stylesheet" href="libs/bootstrap/css/bootstrap.min.css" type="text/css">
+	<link rel="stylesheet" href="libs/feather-font/css/iconfont.css" type="text/css">
+	<link rel="stylesheet" href="libs/icomoon-font/css/icomoon.css" type="text/css">
+	<link rel="stylesheet" href="libs/font-awesome/css/font-awesome.css" type="text/css">
+	<link rel="stylesheet" href="libs/wpbingofont/css/wpbingofont.css" type="text/css">
+	<link rel="stylesheet" href="libs/elegant-icons/css/elegant.css" type="text/css">
+	<link rel="stylesheet" href="libs/slick/css/slick.css" type="text/css">
+	<link rel="stylesheet" href="libs/slick/css/slick-theme.css" type="text/css">
+	<link rel="stylesheet" href="libs/mmenu/css/mmenu.min.css" type="text/css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+	<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+	<link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script> -->
+
+
+	<!-- Site Stylesheet -->
+	<link rel="stylesheet" href="assets/css/app.css" type="text/css">
+	<link rel="stylesheet" href="assets/css/responsive.css" type="text/css">
+	<link rel="stylesheet" href="assets/css/toast.css" type="text/css">
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+
+	<script src="assets/js/toast.js"></script>
+
+
+	<!-- Google Web Fonts -->
+	<link href="https://fonts.googleapis.com/css2?family=Barlow+Semi+Condensed:wght@100;200;300;400;500;600;700&amp;display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=EB+Garamond:100,100italic,200,200italic,300,300italic,400,400italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic&amp;display=swap" rel="stylesheet">
+</head>
+
+
+<body class="home home-2 title-2">
+	<div id="toast"></div>
+	<div id="page" class="hfeed page-wrapper"> 
