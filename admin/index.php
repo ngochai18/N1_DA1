7d12b6f36f4b08bd4d870b457005c88caee16c40
donltqph29825 <dontlqph29825@fpt.edu.vn>
2024-07-22 15:09:13 +0700

    update index.php, global.php, login.php

--- admin/index.php
+++ admin/index.php
@@ -5,11 +5,19 @@ require "../model/pdo.php";
 require "../model/product.php";
 require "../model/category.php";
 require "../model/user.php";
-require "";
+require "../global.php";
 require "../model/comment.php";
 
 ob_start();
 if (!isset($_SESSION['user_admin'])) {
     include "login/login.php";
 } else {
+    include "view/header.php";
+
+    if(isset($_GET['act'])){
+
+        $act = $_GET['act'];
+        
+    }
+    
 }
